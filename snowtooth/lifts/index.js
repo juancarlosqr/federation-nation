259de5539fc5dfd2b72f3e23bda91bc1b5ea55ed
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2021-11-12 07:38:59 +0100

    Make lifts and trails subgraphs

--- snowtooth/lifts/index.js
+++ snowtooth/lifts/index.js
@@ -1,5 +1,6 @@
-const { ApolloServer, gql } = require("apollo-server");
-const lifts = require("./lift-data.json");
+const { ApolloServer, gql } = require('apollo-server');
+const { buildSubgraphSchema } = require('@apollo/subgraph');
+const lifts = require('./lift-data.json');
 
 const typeDefs = gql`
   type Lift {
@@ -47,8 +48,10 @@ const resolvers = {
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema: buildSubgraphSchema({
+    typeDefs,
+    resolvers,
+  }),
 });
 
 server.listen(process.env.PORT).then(({ url }) => {
